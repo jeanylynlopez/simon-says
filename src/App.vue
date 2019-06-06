6049118531bec4d973593d867a0de746594feb24
Jeanylyn Lopez <freelance.jeanylyn.lopez@invivo.com>
2019-06-06 15:10:41 -0400

    add new files for project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <SimonSays />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import SimonSays from './components/SimonSays.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    SimonSays
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+@import "@/styles/_variables.scss"; 
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +29,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  /* color: $red; */
+
 }
 </style>
---
+++ src/components/SimonSays.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <section>
+      <div class="button green">Green</div>
+      <div class="button red">Red</div>
+    </section>  
+    <section>
+      <div class="button yellow">Yellow</div>
+      <div class="button blue">Blue</div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+// NOTES
+// We need to randomize the buttons that simon chooses 
+// Store those randomized buttons into a variable in data
+// Show the user which buttons 
+// on click on each of the buttons 
+// when it is clicked the function will add to a data variable 
+// 
+
+export default {
+  name: 'SimonSays',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+  @import "@/styles/_variables.scss";
+
+  .container {
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+  }
+  
+  // button styles
+  .button {
+    width: 200px;
+    height: 200px;
+    margin: 10px 5px;
+  }
+
+  .green {
+    background-color: $green;
+  }
+
+  .red {
+    background-color: $red;
+  }
+
+  .yellow {
+    background-color: $yellow;
+  }
+
+  .blue {
+    background-color: $blue;
+  }
+
+</style>
